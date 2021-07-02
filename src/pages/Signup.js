ed133316a88a86b480f87265322d57c1eb992643
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-07-03 06:30:31 +0800

    Fixed title alignment with relation to form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,31 +10,29 @@ const { TabPane } = Tabs;
 
 function Signup() {
   return (
-    <Row span={24}>
-      <Col xs={24}>
-        <Row span={24}>
-          <Col xs={24}>
+    <Row span={24} justify="center">
+      <Col xs={24} md={16}>
+        <Row span={24} justify="start">
+          <Col xs={12}>
             <Title level={2}>Sign Up</Title>
           </Col>
-          <Col xs={24}>
+        </Row>
+        <Row span={24} justify="start">
+          <Col xs={12}>
             <p>Step 1</p>
             <p>
               <span>*</span>All fields are necessary
             </p>
           </Col>
         </Row>
-        <Row span={24} justify="center">
-          <Col xs={24} md={16}>
-            <Tabs type="card" defaultActiveKey="2">
-              <TabPane tab="Sign Up as Doctor" key="1">
-                <DoctorsForm />
-              </TabPane>
-              <TabPane tab="Sign Up as Patient" key="2">
-                <PatientsForm />
-              </TabPane>
-            </Tabs>
-          </Col>
-        </Row>
+        <Tabs type="card" defaultActiveKey="2">
+          <TabPane tab="Sign Up as Doctor" key="1">
+            <DoctorsForm />
+          </TabPane>
+          <TabPane tab="Sign Up as Patient" key="2">
+            <PatientsForm />
+          </TabPane>
+        </Tabs>
       </Col>
     </Row>
   );
